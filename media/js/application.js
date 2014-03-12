f2f208afa16fd8299dac38f8207a174d984f145f
hijiangtao <hijiangtao@gmail.com>
2014-03-12 20:24:48 +0800

    AI modify

--- media/js/application.js
+++ media/js/application.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Wait till the browser is ready to render the game (avoids glitches)
-  window.requestAnimationFrame(function () {
-    var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
-  });
+animationDelay = 100;
+minSearchTime = 100;
+
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
 });
